85de153b2326fd1cbc5ad9ce29c95202ac4e43ed
Alla Varavina <fistawka27@gmail.com>
2024-06-08 16:27:59 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+
+const axiosInstanse = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+  timeout: 1000,
+  validateStatus: () => true,
+});
+
+describe("JSONPlaceholder", () => {
+  //test1
+  test("GET /posts status code is 200", async () => {
+    const response = await axiosInstanse.get("/posts");
+    expect(response.status).toBe(200);
+  });
+
+  //test2
+  test("GET /posts/1/comments status code is 200", async () => {
+    const response = await axiosInstanse.get("/posts/1/comments");
+    expect(response.status).toBe(200);
+  });
+
+  //test3
+  test("GET /posts/1 title is equels", async () => {
+    const response = await axiosInstanse.get("/posts/1");
+    expect(response.data).toHaveProperty(
+      "title",
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+    );
+  });
+
+  //test4
+  test("GET /albums/2 userId = 1", async () => {
+    const response = await axiosInstanse.get("/albums/2");
+    expect(response.data).toHaveProperty("userId", 1);
+  });
+
+  //test5
+  test("POST /posts check response id = 101", async () => {
+    const response = await axiosInstanse.post("/posts", {
+      body: {
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 2,
+      },
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    expect(response.data).toHaveProperty("id", 101);
+  });
+});
